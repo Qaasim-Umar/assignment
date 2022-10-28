c2e83a28cfb03b9a21a97be2ca4fdce312f99198
Qaasim Umar <omayr010@gmail.com>
2022-10-28 07:03:53 +0100

    added week 5

--- week 4/js/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#bars');
-let navb =document.querySelector('.nav-links') ;
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navb.classList.toggle('active');
-}
